c7e0ecbcd6c961298c22ad5ec6152ac8dbce82e0
Wingo <cmwingo@gmail.com>
2017-01-20 21:27:48 -0800

    Importing packages but still getting errors

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewMealComponent } from './new-meal.component';
 import { MealListComponent } from './meal-list.component';
 import { EditMealComponent } from './edit-meal.component';
-import { CaloriesPipe } from './calories.pipe'
+import { CaloriesPipe } from './calories.pipe';
 
 @NgModule({
   imports: [ BrowserModule,
-              FormsModule],
+              FormsModule,
+              ReactiveFormsModule],
   declarations: [ AppComponent,
                     NewMealComponent,
                     MealListComponent,
--- app/new-meal.component.ts
+++ app/new-meal.component.ts
@@ -1,4 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Meal } from './meal.model';
 
 @Component({
@@ -6,25 +7,45 @@ import { Meal } from './meal.model';
   template: `
     <h1>New Meal</h1>
     <hr>
-    <div class="form-group">
-      <label>Enter Meal Name:</label>
-      <input #newName class="form-control">
-      <label>Enter Meal Details:</label>
-      <input #newDetails class="form-control">
-      <label>Enter Meal Calories:</label>
-      <input #newCalories class="form-control">
-    </div>
-    <div>
-      <button (click)="submitForm(newName.value, newDetails.value, newCalories.value); newName.value=''; newDetails.value=''; newCalories.value='';">Add</button>
-    </div>
+    <form [formGroup]="newMealForm" (ngSubmit)="submitForm(complexForm.value)">
+      <div class="form-group">
+        <label>Enter Meal Name:</label>
+        <input #newName class="form-control" placeholder="Your meal here" [formControl]="newMealForm.controls['mealName']">
+      </div>
+      <div class="form-group">
+        <label>Enter Meal Details:</label>
+        <input #newDetails class="form-control" placeholder="Details about your meal" [formControl]="newMealForm.controls['mealDetails']">
+      </div>
+      <div class="form-group">
+        <label>Enter Meal Calories:</label>
+        <input #newCalories class="form-control" placeholder="Calories" [formControl]="newMealForm.controls['mealCalories']">
+      </div>
+      <div class="form-group">
+        <button type="submit" class="button btn-default">Submit</button>
+      </div>
+    </form>
   `
 })
 
 export class NewMealComponent {
-  @Output() newMealSender = new EventEmitter();
+  // @Output() newMealSender = new EventEmitter();
 
-  submitForm(name: string, details: string, calories: number) {
-    var newMealToAdd: Meal = new Meal(name, details, calories);
-    this.newMealSender.emit(newMealToAdd);
+  newMealForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.newMealForm = fb.group({
+      'mealName': "",
+      'mealDetails': "",
+      'mealCalories': ""
+    })
   }
+
+  submitForm(value: any){
+    console.log(value);
+  }
+
+  // submitForm(name: string, details: string, calories: number) {
+  //   var newMealToAdd: Meal = new Meal(name, details, calories);
+  //   this.newMealSender.emit(newMealToAdd);
+  // }
 }
